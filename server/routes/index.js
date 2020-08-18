147b01c210ddbf6f8f3824690df5a5b6a4959a52
why <1224125412@qq.com>
2020-08-18 22:58:07 +0800

    use the feature redirect

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-module.exports = router;
+	res.render('index', { title: 'Express' })
+})
+router.get('/error', function(req, res, next) {
+	res.render('error', { status: 200, stack: '123' })
+})
+module.exports = router
--- server/routes/users.js
+++ server/routes/users.js
@@ -4,7 +4,8 @@ var router = express.Router()
 const User = require('../model/user')
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-	res.send('respond with a resource')
+	// res.send('respond with a resource')
+	res.redirect('https://www.baidu.com')
 })
 
 router.post('/register', (req, res) => {
@@ -12,6 +13,29 @@ router.post('/register', (req, res) => {
 	user.save((err, doc) => {
 		if (err) return res.json({ success: false, err })
 		return res.status(200).json({ success: true })
+	})
+})
+
+router.post('/login', (req, res) => {
+	User.findOne({ email: req.body.email }, (err, user) => {
+		if (!user)
+			return res.json({
+				loginSuccess: false,
+				message: 'Auth failed, email not found'
+			})
+
+		user.comparePassword(req.body.password, (err, isMatch) => {
+			if (!isMatch) return res.json({ loginSuccess: false, message: 'Wrong password' })
+
+			user.generateToken((err, user) => {
+				if (err) return res.status(400).send(err)
+				res.cookie('w_authExp', user.tokenExp)
+				res.cookie('w_auth', user.token).status(200).json({
+					loginSuccess: true,
+					userId: user._id
+				})
+			})
+		})
 	})
 })
 
