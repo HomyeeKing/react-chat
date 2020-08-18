ff27fe991b32f894d90164a20d87f2449029d191
why <1224125412@qq.com>
2020-08-18 22:33:53 +0800

    server db config

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
+const User = require('../model/user')
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+	res.send('respond with a resource')
+})
 
-module.exports = router;
+router.post('/register', (req, res) => {
+	const user = new User(req.body)
+	user.save((err, doc) => {
+		if (err) return res.json({ success: false, err })
+		return res.status(200).json({ success: true })
+	})
+})
+
+module.exports = router
